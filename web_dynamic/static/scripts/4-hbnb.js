dbf0b0bc3b7b0d44ff5fb402046918cab7f4f1d0
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-01-15 16:27:46 +0100

    fix: include checkbox functionality to select Amenities

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -5,10 +5,39 @@ let clicks = 0;
 
 function executeScripts () {
   alert('Document Ready');
+  selectAmenities();
   apiStatus();
   fetchPlaces();
   getAmenity();
   getPlaceByAmenityBtn();
+}
+
+function selectAmenities () {
+  const checkeds = new Set();
+  let clicks = 0;
+  $('input[type="checkbox"]').click((event) => {
+    clicks++;
+    const text = $('div.amenities h4').text();
+    const name = event.target.dataset.name;
+    let nameArray;
+
+    if (event.target.checked) {
+      checkeds.add(event.target.dataset.id);
+      nameArray = text.split(', ');
+      if (clicks === 1) { nameArray.length = 0; }
+      nameArray.push(name);
+      $('div.amenities h4').text(nameArray.join(', '));
+    } else {
+      checkeds.delete(event.target.dataset.id);
+      const mainName = $('div.amenities h4').text();
+      const nameSet = new Set(mainName.split(', '));
+      nameSet.delete(name);
+      nameArray = Array.from(nameSet);
+      $('div.amenities h4').text(nameArray.join(', '));
+    }
+    console.log(nameArray);
+    console.log(checkeds);
+  });
 }
 
 function apiStatus () {
